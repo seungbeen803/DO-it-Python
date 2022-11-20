05a6fd9b50a8b21304971dc01f25c8d8ce20cef2
seungbeen <seungbin803@gmail.com>
2022-11-21 00:36:40 +0900

    if __name__ == "__main__"

--- 20221120/실행문과 함수, 클래스 정의가 같이 있는 모듈/gugudan2.py
+++ 20221120/실행문과 함수, 클래스 정의가 같이 있는 모듈/gugudan2.py
@@ -1,1 +1,8 @@
 # gugudan2.py
+def gugudan2(n):
+    print(n, "단을 출력합니다.")
+    for i in range(1, 10):
+        print(n, "x", i, "=", n * i)
+        
+    if __name__ == "__main__":
+        gugudan(3)
---
+++ 20221120/실행문과 함수, 클래스 정의가 같이 있는 모듈/gugudan_main2.py
@@ -0,0 +1,9 @@
+# gugudan2_main.py
+import gugudan2
+def main():
+    for i in range(9, 10):
+        print("="*20)
+    gugudan2.gugudan2(i)
+# 실행이 안되게 막을 수 있다
+if __name__ == "__main__":
+    main()
