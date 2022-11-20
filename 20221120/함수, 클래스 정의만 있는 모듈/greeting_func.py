180fdf8ece3ee237c374b5b3b0f7883bfa07ef45
seungbeen <seungbin803@gmail.com>
2022-11-21 00:23:16 +0900

    함수, 클래스 정의만 있는 모듈

---
+++ 20221120/함수, 클래스 정의만 있는 모듈/greeting_func.py
@@ -0,0 +1,4 @@
+# greeting_func.py
+def greeting():
+    print("greeting_func 모듈입니다.")
+    print("__name__ : ", __name__)
---
+++ 20221120/함수, 클래스 정의만 있는 모듈/hello_func.py
@@ -0,0 +1,4 @@
+# hello_func.py
+def hello():
+    print("hello_func 모듈입니다.")
+    print("Hello World?")
