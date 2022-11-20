b1deeff5ec6dd75aa710beb2407ec9d3b689c689
seungbeen <seungbin803@gmail.com>
2022-11-21 00:47:06 +0900

    if __name__ == "__main__":

--- 20221120/실행문과 함수, 클래스 정의가 같이 있는 모듈/gugudan_main2.py
+++ 20221120/실행문과 함수, 클래스 정의가 같이 있는 모듈/gugudan_main2.py
@@ -3,7 +3,8 @@ import gugudan2
 def main():
     for i in range(9, 10):
         print("="*20)
-    gugudan2.gugudan2(i)
-# 실행이 안되게 막을 수 있다
+    gugudan2.gugudan(i)
+# 메인 모듈로 실행될 경우 __name__ 내장 변수가 __main__으로 설정되는 것을
+# 이용하여 메인 모듈로 실행된 경우에만 gugudan(3) 함수가 호출되도록 하고 있다.
 if __name__ == "__main__":
     main()
